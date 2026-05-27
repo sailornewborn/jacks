e726ff5baa26b9d81bf7b6d329b1389a4da93d87
jack <noemail>
2026-05-27 19:43:45 +0800

    added module [windows_listening_server] and a function [get_LAN_ip_windows]

---
+++ src/jacks/windows_listening_server/__init__.py
@@ -0,0 +1,10 @@
+import psutil
+from socket import AF_INET
+def get_LAN_ip_windows() -> str:
+    possible_ip = None
+    all_interfaces = psutil.net_if_addrs()
+    for name, infos in all_interfaces.items():
+        for set in infos:
+            if set.address.startswith("192.") and set.family == AF_INET:
+                possible_ip = set.address
+    return possible_ip
