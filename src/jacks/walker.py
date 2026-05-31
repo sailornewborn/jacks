be3a767cd88622c23b90b8cca7edd80e3876b346
jack <noemail>
2026-05-31 19:00:08 +0800

    added module walker

---
+++ src/jacks/walker.py
@@ -0,0 +1,14 @@
+from jacks import *
+
+class GetWalker:
+    def __init__(self,start:int,stop:int):
+        self.start = start
+        self.stop = stop
+    
+    def get_walking(self) -> list[int]:
+        holder = []
+        for item in range(self.start,self.stop):
+            holder.append(item)
+        holder.append(self.stop)
+        return holder
+    
